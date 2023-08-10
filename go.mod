71ef16016c55eeb37aeeeb19553f1205baa95fc7
Nicolas Xavier Herrera Medina <nicoxavier2013@hotmail.com>
2023-08-09 22:45:35 -0500

    Subir el primer codigo de Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/NXHM/Golang-freecodecamp
+
+go 1.21.0
---
+++ hello-world.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// los comentarios inline son con //
+	fmt.Println("Hello World")
+}
